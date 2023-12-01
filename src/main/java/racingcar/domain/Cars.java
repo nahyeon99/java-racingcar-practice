9ffd3dbb14767a5fd222f827d1e01c08746fb2c6
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 12:42:54 +0900

    feat(domain/Cars): Cars 클래스 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import java.util.List;
+import racingcar.dto.MatchDto;
+import racingcar.dto.NameDto;
+import racingcar.util.MoveTypeGenerator;
+
+public class Cars {
+    private final List<Car> cars;
+
+    private Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars of(final List<Car> cars) {
+        return new Cars(cars);
+    }
+
+    public List<MatchDto> match() {
+        return cars.parallelStream()
+                .map(car -> {
+                    MoveType moveType = MoveTypeGenerator.generate();
+                    return car.move(moveType);
+                })
+                .toList();
+    }
+
+    public List<NameDto> getMaxPositionCars() {
+        Car maxPositionCar = getMaxPositionCar();
+
+        return cars.stream()
+                .filter(car -> !maxPositionCar.equals(car) &&
+                        maxPositionCar.isEqualMaxPosition(car))
+                .map(NameDto::from)
+                .toList();
+    }
+
+    private Car getMaxPositionCar() {
+        return cars.stream()
+                .max(Car::compareTo)
+                .orElse(cars.get(0));
+    }
+}
