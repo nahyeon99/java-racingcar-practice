435b0371df8bf014cb969726753e43a810c53b02
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 11:14:15 +0900

    feat(domain/Name): 자동차 이름 클래스 추가

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import static racingcar.exception.ExceptionMessage.INVALID_NAME_FORMAT;
+import static racingcar.exception.ExceptionMessage.INVALID_NAME_SIZE;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Name {
+    private static final Pattern carNamePattern = Pattern.compile("^[a-z]*$");
+    private static final int MIN_NAME_SIZE = 1;
+    private static final int MAX_NAME_SIZE = 5;
+
+    private final String name;
+
+    private Name(String name) {
+        validateNameSize(name);
+        validateNameFormat(name);
+
+        this.name = name;
+    }
+
+    public static Name from(String name) {
+        return new Name(name);
+    }
+
+    private static void validateNameFormat(String name) {
+        Matcher matcher = carNamePattern.matcher(name);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException(INVALID_NAME_FORMAT.getKorean());
+        }
+    }
+
+    private void validateNameSize(String name) {
+        if (name.length() < MIN_NAME_SIZE || name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException(INVALID_NAME_SIZE.getKorean());
+        }
+    }
+}
