8c4a15210ddc1e9c8d4ca82bace72d3ce551789e
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 11:02:06 +0900

    feat(exception/ExceptionMessage): 예외 처리 기능 추가

---
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -0,0 +1,20 @@
+package racingcar.exception;
+
+public enum ExceptionMessage {
+    INVALID_BLANK("필수 입력 항목입니다."),
+    INVALID_NUMBER_FORMAT("정수로 입력해 주세요."),
+    INVALID_NAME_FORMAT("이름은 영소문자로만 입력해 주세요."),
+    INVALID_NAME_SIZE("이름은 1~5자 내에서 입력해 주세요."),
+    INVALID_TRY_COUNT_SIZE("시도 횟수는 1~15자 내에서 입력해 주세요."),
+    ;
+
+    private final String korean;
+
+    ExceptionMessage(String message) {
+        this.korean = message;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
+}
