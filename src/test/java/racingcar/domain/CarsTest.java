2adfb9e202ea4dc385662fd24c2b5560e5c4f303
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 14:23:06 +0900

    test(domain/CarsTest): 중복된 이름 검증 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,13 +1,31 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.dto.NameDto;
+import racingcar.exception.ExceptionMessage;
 import racingcar.fixture.CarFixture;
 
 class CarsTest {
+
+    @DisplayName("중복된 이름이 존재하면 예외를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"poby, jun"})
+    void fail_by_duplicate_name(String name1, String name2) {
+        List<Car> cars = List.of(CarFixture.createCarBy(name1, 0),
+                CarFixture.createCarBy(name1, 0),
+                CarFixture.createCarBy(name2, 0));
+
+        assertThatThrownBy(() -> Cars.of(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ExceptionMessage.INVALID_DUPLICATE_NAME.getKorean());
+    }
 
     @DisplayName("가장 많이 전진한 Cars를 조회한다.")
     @Test
