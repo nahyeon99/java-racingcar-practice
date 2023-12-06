3dd5b8da4f0d097ad0ce7376ad67e75ad43c00ca
nahyeon99 <nahyeonee99@gmail.com>
2023-12-06 13:34:40 +0900

    feat(view/InputView): 입력 기능 추가

---
+++ src/main/java/racingcar/util/InputUtil.java
@@ -0,0 +1,42 @@
+package racingcar.util;
+
+import static racingcar.exception.ExceptionMessage.INVALID_BLANK;
+import static racingcar.exception.ExceptionMessage.INVALID_NUMBER_FORMAT;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class InputUtil {
+    private InputUtil() {
+        throw new AssertionError("생성자 호출이 불가합니다.");
+    }
+
+    public static int readNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT.getKorean());
+        }
+    }
+
+    public static String readLine() {
+        String input = Console.readLine();
+
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException(INVALID_BLANK.getKorean());
+        }
+
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_BLANK.getKorean());
+        }
+        return input;
+    }
+
+    public static List<String> readValues(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter))
+                .map(String::trim)
+                .toList();
+    }
+}
